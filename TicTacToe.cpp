db706e0815ee97c9520bad938941027a0ef93d15
ppp <ppp>
2021-02-16 13:39:18 +0000

    ppp create display function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,20 +1,33 @@
 #include<iostream>
+#include <iomanip>
 using namespace std;
 void CreateBoard(int board[3][3]){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            board[i][j]=1;
+            board[i][j]=0;
         }
+    }
+}
+void DisplayBoard(int board[3][3])
+{
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            if (board[i][j] == 0){
+                cout <<setw(2)<<"🔲";
+            }else if (board[i][j] == 1){
+                cout <<setw(2)<<"√";
+            }else if (board[i][j] == 2){
+                cout <<setw(2)<<"×";
+            }else{
+                cout <<setw(2)<<"?";
+            }
+        }
+        cout << endl;
     }
 }
 int main(){
     int board[3][3];
     CreateBoard(board);
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cout<<board[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    DisplayBoard(board);
     return 0;
 }
