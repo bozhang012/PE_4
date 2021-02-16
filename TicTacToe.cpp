8442db0f949dfba71544eab6eacdbbdfb10b9c15
bozh <bozh>
2021-02-16 14:36:01 +0000

    all finish

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
+#include <iostream>
 #include <iomanip>
 using namespace std;
-void CreateBoard(int board[3][3]){
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            board[i][j]=0;
+void CreateBoard(int board[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            board[i][j] = 0;
         }
     }
 }
@@ -17,36 +20,40 @@ void DisplayBoard(int board[3][3])
         {
             if (board[i][j] == 0)
             {
-                cout <<setw(2)<<"🟩";
+                cout << setw(2) << "🟩";
             }
             else if (board[i][j] == 1)
             {
-                cout <<setw(2)<<"✅";
+                cout << setw(2) << "✅";
             }
             else if (board[i][j] == 2)
             {
-                cout <<setw(2)<<"❎";
+                cout << setw(2) << "❎";
             }
             else
             {
-                cout <<setw(2)<<"🟥";
+                cout << setw(2) << "🟥";
             }
         }
         cout << endl;
     }
 }
-void PlaceMarker(int &L,int play){
-    L=play;
+void PlaceMarker(int &L, int play)
+{
+    L = play;
 }
-void GetPlayerChoice(int board[3][3],int play){
-    cout<<"***************play_""<<play<<"<<"***************"<<endl;
-    cout<<"Choise row: ";
+void GetPlayerChoice(int board[3][3], int play)
+{
+    cout << "***************play_"
+            <<play
+         << "***************" << endl;
+    cout << "Choise row: ";
     int row;
-    cin>>row;
-    cout<<"Choise col: ";
+    cin >> row;
+    cout << "Choise col: ";
     int col;
-    cin>>col;
-    PlaceMarker(board[row-1][col-1],play);
+    cin >> col;
+    PlaceMarker(board[row - 1][col - 1], play);
 }
 
 int main()
@@ -54,9 +61,11 @@ int main()
     int board[3][3];
     CreateBoard(board);
     DisplayBoard(board);
-    GetPlayerChoice(board,1);
-    DisplayBoard(board);
-
+    for (int i = 0; i < 9; i++)
+    {
+        GetPlayerChoice(board, 1+(i%2));
+        DisplayBoard(board);
+    }
 
     return 0;
 }
